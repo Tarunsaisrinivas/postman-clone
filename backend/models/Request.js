a459dab7a18a22d2ac5699d3bd17419047141092
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2026-01-01 20:30:39 +0530

    model created for the data

---
+++ backend/models/Request.js
@@ -0,0 +1,17 @@
+// models/Request.js
+const mongoose = require("mongoose");
+
+const requestSchema = new mongoose.Schema({
+  name: String,
+  method: String,
+  url: String,
+  headers: Object,
+  params: Object,
+  body: Object,
+  response: Object,
+  status: Number,
+  responseTime: Number,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Request", requestSchema);
